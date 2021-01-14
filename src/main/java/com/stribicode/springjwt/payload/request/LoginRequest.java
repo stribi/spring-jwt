92baf82cad2fa0b195826d008c2f53c4c41b9fed
goran <goranpopovic@outlook.com>
2021-01-14 21:02:05 +0100

    Add payloads for Spring Rest Controller

---
+++ src/main/java/com/stribicode/springjwt/payload/request/LoginRequest.java
@@ -0,0 +1,33 @@
+package com.stribicode.springjwt.payload.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+	
+	@NotBlank
+	@Email
+	private String username;
+	
+	@NotBlank
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/stribicode/springjwt/payload/request/RegisterRequest.java
@@ -0,0 +1,62 @@
+package com.stribicode.springjwt.payload.request;
+
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.stribicode.springjwt.model.Role;
+
+public class RegisterRequest {
+	
+	@NotBlank
+	@Size(min = 3, max =  50)
+	private String fullName;
+	
+	@NotBlank
+	@Email
+	@Size(max = 50)
+	private String username;
+	
+	@NotBlank
+	@Size(min = 6, max = 40)
+	private String password;
+	
+	
+	private Set<Role> role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Set<Role> getRole() {
+		return role;
+	}
+
+	public void setRole(Set<Role> role) {
+		this.role = role;
+	}
+	
+	
+}
